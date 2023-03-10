8bd5db8ccc666f04e2aeb1fc629e7db3edd59b04
ny.yi <2n.nayeon.yi@daum.net>
2023-03-10 13:18:05 +0900

    add default variable and page transition

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -1,11 +1,17 @@
 import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
 
-const pageDefaultStyle = {
-  position: 'absolute',
+const pageDefault = {
   top: '40px',
   left: '40px',
-  width: 'calc(100% - 40px * 2)',
-  height: 'calc(100% - 40px * 2.5)',
+  width() { return `calc(100% - ${this.left} * 2)`},
+  height() { return `calc(100% - ${this.left} * 2.5)`},
+}
+const pageDefaultStyle = {
+  position: 'absolute',
+  top: pageDefault.top,
+  left: pageDefault.left,
+  width: pageDefault.width(),
+  height: pageDefault.height(),
   opacity: 0
 }
 
@@ -40,7 +46,23 @@ export const slideInAnimation = trigger('routeAnimations', [
         animate('500ms ease-in-out', style({ opacity: 0 }))
       ], { optional: true }),
       query(':enter', [
-        animate('500ms ease-in-out', style({ top: '40px', opacity: 1 }))
+        animate('500ms ease-in-out', style({ top: pageDefault.top, opacity: 1 }))
+      ], { optional: true }),
+    ])
+  ]),
+  transition('* <=> *', [
+    style({ position: 'relative' }),
+    query(':enter, :leave', [
+      style(pageDefaultStyle)
+    ], { optional: true }),
+    query(':leave', animateChild(), { optional: true }),
+    query(':enter', style({ left: '50%', opacity: 0 })),
+    group([
+      query(':leave', [
+        animate('500ms ease-in-out', style({ left: '-50%' }))
+      ], { optional: true }),
+      query(':enter', [
+        animate('500ms ease-in-out', style({ left: pageDefault.left, opacity: 1 }))
       ], { optional: true }),
     ])
   ]),
